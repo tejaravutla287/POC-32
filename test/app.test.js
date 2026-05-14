e5132caa9f9b29d25d4ec0b0fcfd158f634eb8d4
tejaravutla287 <tejaravutla287@gmail.com>
2026-05-14 17:51:25 +0530

    Create app.test.js

---
+++ test/app.test.js
@@ -0,0 +1,7 @@
+const assert = require('assert');
+ 
+describe('Basic Web Test', () => {
+  it('should return true to verify pipeline testing stage', () => {
+    assert.strictEqual(1 + 1, 2);
+  });
+});
